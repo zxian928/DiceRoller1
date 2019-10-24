ed9901649baa42fb81d94a4b0fa850f5d48086e1
zxian928 <zhaoxian0928@gmail.com>
2019-10-24 11:12:00 +0800

    DiceRollerFinal

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -12,11 +13,13 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
+    lateinit var diceImage : ImageView
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
+        val rollButton: Button = this.findViewById(R.id.roll_button)
 
         rollButton.setOnClickListener { rollDice() }
 
@@ -24,26 +27,51 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun rollDice() {
-        val total: TextView = findViewById(R.id.total)
 
-        val num1 = Random().nextInt(6) + 1
-        val num2 = Random().nextInt(6) + 1
-        val num3 = Random().nextInt(6) + 1
+        //val randomInt1 = Random().nextInt(6) + 1
+        //val randomInt2 = Random().nextInt(6) + 1
+        //val randomInt3 = Random().nextInt(6) + 1
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        val resultText1: TextView = findViewById(R.id.result_text1)
-        val resultText2: TextView = findViewById(R.id.result_text2)
+        //val drawableResource = when (randomInt1) {
+        //    1 -> R.drawable.dice_1
+        //    2 -> R.drawable.dice_2
+        //    3 -> R.drawable.dice_3
+        //    4 -> R.drawable.dice_4
+        //    5 -> R.drawable.dice_5
+        //    else -> R.drawable.dice_6
+        //}
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val diceImage1: ImageView = findViewById(R.id.dice_image1)
+        val diceImage2: ImageView = findViewById(R.id.dice_image2)
+        //resultText.text = randomInt.toString()
+        diceImage.setImageResource( dice() )
+        diceImage1.setImageResource( dice() )
+        diceImage2.setImageResource( dice() )
+
 
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
+    }
 
-        resultText.text = "Dice Rolled!"
-        resultText.text = num1.toString()
-        resultText1.text = num2.toString()
-        resultText2.text = num3.toString()
-        val sum = num1+num2+num3
-        total.text = "Total = " + sum.toString()
+    private fun dice(): Int{
+        val randomInt1 = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        return drawableResource
 
     }
+
+}
+
+private fun ImageView.setImageResource(function: () -> Int) {
 
 }
